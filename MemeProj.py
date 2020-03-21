5300dc57c90364111d530d01e1a555fea1ba40d8
Rahul Ram <rahulrram04@gmail.com>
2020-03-21 14:43:29 -0400

    added code to measure runtime
    uses the time module (time.time()):
    measures start_time before the bulk of the program, and measures end_time after the bulk of the program
    measures runtime in seconds using the difference between the two times measured (start_time - end_time) and creates a runtime file (.txt format) to record this in a file instead of the shell's console

--- MemeProj.py
+++ MemeProj.py
@@ -4,14 +4,16 @@ import pandas as pd
 import numpy as np
 import datetime
 from sklearn.preprocessing import MinMaxScaler
+import time
 
-person = 'David'
+start_time = time.time()
+
+person = 'Rahul' # choose 'Rahul' or 'David'
 data = pd.read_csv(('{}Grades.csv'.format(person)))
-train = []
-labels = []
+train, labels = [], []
 for x in range(data.shape[0]):
     date = list(data[['Year','Month','Day']].iloc[x])
-    day = int((datetime.date(int(date[0]),int(date[1]),int(date[2])) - datetime.date(2019,9,4)).days)
+    day = int((datetime.date(int(date[0]),int(date[1]),int(date[2])) - datetime.date(2019, 9, 4)).days)
     grade = int(data[['Grade (Decimal)']].iloc[x] * 100)
     total = int(data[['Point Total']].iloc[x])
     weight = int(data[['Weight (0.3/0.7)']].iloc[x] * 100)
@@ -19,13 +21,13 @@ for x in range(data.shape[0]):
     labels.append(grade)
 traindf = pd.DataFrame(train)
 traindf['labels'] = labels
-traindf = traindf.sort_values(by=[0])
-input_data = traindf[[1, 'labels']].reset_index(drop=True)
+traindf = traindf.sort_values(by = [0])
+input_data = traindf[[1, 'labels']].reset_index(drop = True)
 
-sc= MinMaxScaler(feature_range=(0,1))
+sc = MinMaxScaler(feature_range = (0, 1))
 input_data = sc.fit_transform(input_data)
 x_train, y_train = [], []
-lookback = 10
+lookback = 10 # number of previous grades the neural network will process
 features = 2
 for i in range(input_data.shape[0] - lookback - 1):
     t = []
@@ -37,22 +39,28 @@ for i in range(input_data.shape[0] - lookback - 1):
 x_train, y_train = np.array(x_train), np.array(y_train)
 x_train = x_train.reshape(x_train.shape[0], lookback, features)
 preds, future_preds = [], []
-for x in range(10):
+n = 100 # number of trials to run the model (more trials --> more accurate estimates)
+for x in range(n):
     model = Sequential()
     model.add(LSTM(50, input_shape=(lookback, features)))
     model.add(Dense(8, activation='relu'))
     model.add(Dropout(0.15))
     model.add(Dense(1))
-    model.compile(loss='mean_squared_error', optimizer='adam')
-
-    model.fit(x_train,y_train, batch_size=16, epochs=500)
+    model.compile(loss = 'mean_squared_error', optimizer = 'adam')
+    model.fit(x_train,y_train, batch_size = 16, epochs = 500)
     preds.append((model.predict(x_train)))
-    future_preds.append(model.predict(input_data[-1:-(lookback+1):-1][::-1].reshape(1, lookback, features)))
-preds, avg_future = np.array(preds), sum(np.array(future_preds))/10
-avgpreds = [sum(preds[:,i])/10 for i in range(preds.shape[1])]
+    future_preds.append(model.predict(input_data[-1:-(lookback + 1):-1][::-1].reshape(1, lookback, features)))
+preds, avg_future = np.array(preds), sum(np.array(future_preds)) / n
+avgpreds = [sum(preds[:,i]) / n for i in range(preds.shape[1])]
 avgpreds = np.array(avgpreds)
 avgpreds = list(avgpreds.reshape(avgpreds.shape[0]))
 preddf = pd.DataFrame(y_train)
 preddf['preds'] = avgpreds
 print(avg_future)
-preddf.to_csv(('{}preds.csv'.format(person)), index='False')
+preddf.to_csv(('{}preds.csv'.format(person)), index = 'False')
+
+end_time = time.time()
+
+rt = open("{}Runtime.txt".format(person), mode = 'w+')
+rt.write(str((("Total execution time: {}".format(end_time - start_time)))) + " seconds")
+rt.close()
